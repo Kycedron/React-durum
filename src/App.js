344f687d6544f205d37ed372d69274c4c39e3e5e
Kycedron <138807475+Kycedron@users.noreply.github.com>
2024-02-08 17:33:19 +0300

    sort pop up

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import './scss/app.scss';
 
-import Header from './Components/Header';		
-import Categories from './Components/Categories';			
-import Sort from './Components/Sort';		
-import DurumBlock from './Components/DurumBlock';			
+import Header from './Components/Header';
+import Categories from './Components/Categories';
+import Sort from './Components/Sort';
+import DurumBlock from './Components/DurumBlock';
 import Footer from './Components/Footer/Footer';
 import Swiper from './Components/SwiperComp';
 
-import durum from './assets/durum.json'
+import durum from './assets/durum.json';
 
 function App() {
   return (
-<body>
-	<noscript>You need to enable JavaScript to run this app.</noscript>
-	<div id="root">
-		<div className="wrapper">
-			<Header/>
-			<Swiper/>
-			<div className="content">
-				<div className="container">
-					<div className="content__top">
-					<Categories/>
-					<Sort/>
-					</div>
-					<h2 className="content__title">Все durumы</h2>
-					<div className="content_items">
-					{
-						durum.map((obj)=>(
-						<DurumBlock {... obj} />
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<Footer/>
-</body>
+    <body>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root">
+        <div className="wrapper">
+          <Header />
+          <Swiper />
+          <div className="content">
+            <div className="container">
+              <div className="content__top">
+                {/* <Categories/> */}
+                <Sort />
+              </div>
+              <h2 className="content__title">Все durumы</h2>
+              <div className="content_items">
+                {durum.map((obj) => (
+                  <DurumBlock key={obj.id} {...obj} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </body>
   );
 }
 
