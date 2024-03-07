4b4f843ecb1c1edc678a122bdac9955e34c7d7d0
Kycedron <138807475+Kycedron@users.noreply.github.com>
2024-03-07 18:47:25 +0300

    fixing black themeS

---
+++ src/redux/slices/durumSlice.js
@@ -0,0 +1,20 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+//   items: [],
+// };
+// durum.items;
+
+// const durumSlice = createSlice({
+//   name: 'durum',
+//   initialState,
+//   reducers: {
+//     setItems(state, action) {
+//       state.items = action.payload;
+//     },
+//   },
+// });
+
+// export const { setItems } = durumSlice.actions;
+
+// export default durumSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filter from './slices/filterSlice'
 import cart from './slices/cartSlice'
+import durum from './slices/durumSlice'
+import themeReducer from './slices/themeSlice';
 
 export const store = configureStore({
   reducer: { 
     filter,
-    cart
+    cart,
+    durum,
+    theme: themeReducer,
   },
 })
 
